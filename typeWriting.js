9e05b234f90841811947228aead02015f2bfb349
Billal Boudlal <Billalboudlal2210@outlook.fr>
2026-01-22 15:56:57 +0100

    Ajout de l'animation typeWriting en JS. fichier "typeWriting.js"

---
+++ typeWriting.js
@@ -0,0 +1,40 @@
+const mots = ["Choisis", "Scanne", "Teste", "Maîtrise"];
+let motIndex = 0;
+let lettreIndex = 0;
+let isDeleting = false;
+//-------------------------------------------------//
+//-------------------Variable ID-------------------//
+//-------------------------------------------------//
+const dynamique = document.getElementById("mot-dynamique");
+//-------------------------------------------------//
+//--------------------Function---------------------//
+//-------------------------------------------------//
+const typeEffect = () => {
+  const currentWord = mots[motIndex];
+  let typeSpeed = 150;
+
+  if (isDeleting) {
+    lettreIndex--;
+    typeSpeed = 75;
+  } else {
+    lettreIndex++;
+  }
+
+  dynamique.textContent = currentWord.slice(0, lettreIndex);
+
+  if (isDeleting === false && lettreIndex === currentWord.length) {
+    isDeleting = true;
+    typeSpeed = 2000;
+  } else if (isDeleting === true && lettreIndex === 0) {
+    isDeleting = false;
+    motIndex++;
+    typeSpeed = 500;
+  }
+
+  if (motIndex === mots.length) {
+    motIndex = 0;
+  }
+
+  setTimeout(typeEffect, typeSpeed);
+};
+typeEffect();
